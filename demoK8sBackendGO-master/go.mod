267e69919c36f7c326db46214f118afdf579bd7d
GOKUL41602 <gokul.18it@kct.ac.in>
2022-01-24 12:47:12 +0530

    date stored into pod volume

---
+++ demoK8sBackendGO-master/go.mod
@@ -0,0 +1,45 @@
+module example.com/m
+
+go 1.17
+
+require (
+	github.com/denisenkom/go-mssqldb v0.11.0
+	github.com/go-sql-driver/mysql v1.6.0
+)
+
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/tklauser/numcpus v0.3.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+)
+
+require (
+	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/rs/cors v1.8.2
+	go.mongodb.org/mongo-driver v1.8.2
+	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
+)
+
+require (
+	github.com/Azure/azure-storage-file-go v0.8.0
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
+
+require (
+	github.com/Azure/azure-pipeline-go v0.2.1 // indirect
+	github.com/mattn/go-ieproxy v0.0.0-20190610004146-91bb50d98149 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible
+	github.com/tklauser/go-sysconf v0.3.9
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/sys v0.0.0-20210816074244-15123e1e1f71 // indirect
+)
---
+++ demoK8sBackendGO-master/main.go
@@ -0,0 +1,333 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/Azure/azure-storage-file-go/azfile"
+	_ "github.com/denisenkom/go-mssqldb"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/mem"
+)
+
+type SysInfo struct {
+	Hostname string `bson:hostname`
+	Platform string `bson:platform`
+	CPU      string `bson:cpu`
+	RAM      uint64 `bson:ram`
+	Disk     uint64 `bson:disk`
+}
+
+func sysInfo() *SysInfo {
+
+	hostStat, _ := host.Info()
+	cpuStat, _ := cpu.Info()
+	vmStat, _ := mem.VirtualMemory()
+	diskStat, _ := disk.Usage("\\") // If you're in Unix change this "\\" for "/"
+
+	info := new(SysInfo)
+
+	info.Hostname = hostStat.Hostname
+	info.Platform = hostStat.Platform
+	info.CPU = cpuStat[0].ModelName
+	info.RAM = vmStat.Total / 1024 / 1024
+	info.Disk = diskStat.Total / 1024 / 1024
+
+	fmt.Printf("%+v\n", info)
+	return info
+}
+func main() {
+	Sqldb = sqlDB()
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/title", getTitle).Methods("GET")
+	r.HandleFunc("/api/sign-up", signUp).Methods("POST")
+	r.HandleFunc("/api/file-upload", fileUpload).Methods("POST")
+
+	r.HandleFunc("/api/login", login).Methods("POST")
+
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+	})
+	handler := c.Handler(r)
+	srv := &http.Server{Handler: handler, Addr: ":3000"}
+	log.Fatal(srv.ListenAndServe())
+}
+
+type Data struct {
+	Title string `json: "title"`
+}
+type UserData struct {
+	Username string `json: "username"`
+	Email    string `json: "email"`
+	Password string `json: "password"`
+	Id       int    `json:_id`
+}
+type fileDetails struct {
+	File string `json: file`
+	Type string `json: type`
+	Name string `json: name`
+}
+type FileMetaData struct {
+	UserId       int    `json:userId`
+	UserName     string `json :userName`
+	Email        string `json :email`
+	UploadedTime string `json :uploadedTime`
+	FileName     string `json:fileName`
+	FileFormat   string `json :fileFormat`
+}
+type StatusData struct {
+	StatusData string `json: "statusData"`
+	Id         int    `json :id`
+	Name       string `json:name`
+	Email      string `json:email`
+}
+
+type Sizer interface {
+	Size() int64
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	title := Data{"Devops Demo"}
+	//user := UserData{"d", "d", "d"}
+	json.NewEncoder(w).Encode(&title)
+}
+
+func fileUpload(w http.ResponseWriter, r *http.Request) {
+
+	file, fileHeader, _ := r.FormFile("file")
+
+	size := file.(Sizer).Size()
+	byteContainer := make([]byte, size)
+
+	file.Read(byteContainer)
+
+	f, err := os.Create("/usr/share/app/" + fileHeader.Filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	val := string(byteContainer)
+	// fmt.Println("val: ", val)
+	data := []byte(val)
+	_, err2 := f.Write(data)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	url := getFileURL(fileHeader.Filename)
+	//  main1(f)
+	fmt.Println(url)
+
+	var statusData StatusData
+
+	statusData.StatusData = "success"
+	//info := sysInfo()
+	name, err := os.Hostname()
+	statusData.Email = url + " " + name
+
+	json.NewEncoder(w).Encode(statusData)
+
+}
+func signUp(w http.ResponseWriter, r *http.Request) {
+	//main1()
+	Sqldb = sqlDB()
+	fmt.Println("POST METHOD")
+	w.Header().Set("Content-Type", "application/json")
+	var userData UserData
+	//var book Book
+	_ = json.NewDecoder(r.Body).Decode(&userData)
+	fmt.Println(userData)
+	fmt.Println("UserName: " + userData.Username)
+	// sqlStatement, err := db.Prepare("INSERT INTO inventory (name, quantity) VALUES (?, ?);")
+	// res, err := sqlStatement.Exec("banana", 150)
+	stmt, err1 := Sqldb.Prepare("insert into userdetails(email,password,name) values(?, ?, ?);")
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	res, err1 := stmt.Exec(userData.Email, userData.Password, userData.Username)
+	fmt.Println("res=  -====-=-=-=-=-=-=", res)
+
+	var statusData StatusData
+
+	statusData.StatusData = "success"
+
+	json.NewEncoder(w).Encode(statusData)
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	Sqldb = sqlDB()
+	var userData UserData
+	_ = json.NewDecoder(r.Body).Decode(&userData)
+	var userName string = userData.Username
+	var pass string = userData.Password
+
+	fmt.Println("user values: ", userData)
+	fmt.Println("userName : " + userData.Username)
+
+	// ctx := context.Background()
+	fmt.Println("login method")
+	w.Header().Set("Content-Type", "application/json")
+
+	//var book Book
+	// _ = json.NewDecoder(r.Body).Decode(&userData)
+
+	// Read employees
+	var stat string = " "
+	var name, email, password string
+	// rows,err := Sqldb.Query("select * from userdetails where name=?")
+	rows, err := Sqldb.Query("select * from userdetails where name= ? and password= ?", userName, pass)
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&email, &password, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if userData.Username == userName {
+			stat = "success"
+
+			break
+		}
+		// fmt.Println(name)
+		fmt.Println("Name : ", name)
+
+	}
+	if stat == " " {
+		stat = "failure"
+	}
+
+	defer Sqldb.Close()
+	fmt.Println("status : ", stat)
+
+	var statusData StatusData
+	if stat == "failure" {
+		fmt.Println("stat : ", stat)
+		statusData.StatusData = "failure"
+		json.NewEncoder(w).Encode(statusData)
+	}
+
+	if stat == "success" {
+		fmt.Print("status : ", stat)
+		statusData.StatusData = "success"
+		json.NewEncoder(w).Encode(statusData)
+	}
+
+}
+
+var Sqldb *sql.DB
+
+func sqlDB() *sql.DB {
+	var server = "akswebappdb21.mysql.database.azure.com"
+	// var port = 3306
+
+	var database = "usertable"
+	var user = "dbadminuser@akswebappdb21"
+	var password = "Maxmin@321"
+	// var user = os.Getenv("DB_USERNAME")
+	// var password = os.Getenv("DB_PASSWORD")
+	connString := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", user, password, server, database)
+
+	var err error
+
+	// Create connection pool
+	Sqldb, err = sql.Open("mysql", connString)
+	if err != nil {
+		log.Fatal("Error creating connection pool: ", err.Error())
+	}
+	if Sqldb != nil {
+		fmt.Printf("Connected!\n")
+	}
+
+	return Sqldb
+
+}
+
+func accountInfo() (string, string) {
+	return os.Getenv("ACCOUNT_NAME"), os.Getenv("ACCOUNT_KEY")
+	// return "fc29f08dfb238435fbcf4ec", "RayUPDk/VRZ80/2OheDHJWZg76htfobGy5Me2PpsNOKh5m4+Glh0PNNem/YbZ0MRNtUMbxtG7VE6g/hByq9JdQ=="
+}
+
+func getFileURL(fileName string) string {
+
+	accountName, accountKey := accountInfo()
+	credential, err := azfile.NewSharedKeyCredential(accountName, accountKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	u, _ := url.Parse(fmt.Sprintf("https://%s.file.core.windows.net/kubernetes-dynamic-pvc-7853f4b1-0047-4270-a19e-bc97e2d4ccb6/%s", accountName, fileName))
+	vars := fmt.Sprintf("https://%s.file.core.windows.net/kubernetes-dynamic-pvc-7853f4b1-0047-4270-a19e-bc97e2d4ccb6/%s", accountName, fileName)
+	fileURL := azfile.NewFileURL(*u, azfile.NewPipeline(credential, azfile.PipelineOptions{}))
+
+	fmt.Println("File URL: ", fileURL)
+	return vars
+}
+
+// func main1(file *os.File) string {
+// 	//file, err := os.Open("BigFile.bin") // Open the file we want to upload (we assume the file already exists).
+// 	// file, err := os.Create("Sample.txt")
+// 	// fmt.Println(file.Name())
+
+// 	defer file.Close()
+// 	fileSize, err := file.Stat() // Get the size of the file (stream)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	// From the Azure portal, get your Storage account file service URL endpoint.
+// 	accountName, accountKey := accountInfo()
+// 	credential, err := azfile.NewSharedKeyCredential(accountName, accountKey)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	// Create a FileURL object to a file in the share (we assume the share already exists).
+// 	//https://%s.file.core.windows.net/kubernetes-dynamic-pvc-7853f4b1-0047-4270-a19e-bc97e2d4ccb6/%s
+// 	u, _ := url.Parse(fmt.Sprintf("https://%s.file.core.windows.net/myshare/%s", accountName, file.Name()))
+// 	vars := fmt.Sprintf("https://%s.file.core.windows.net/myshare/%s", accountName, file.Name())
+// 	fileURL := azfile.NewFileURL(*u, azfile.NewPipeline(credential, azfile.PipelineOptions{}))
+
+// 	ctx := context.Background() // This example uses a never-expiring context
+
+// 	// Trigger parallel upload with Parallelism set to 3. Note if there is an Azure file
+// 	// with same name exists, UploadFileToAzureFile will overwrite the existing Azure file with new content,
+// 	// and set specified azfile.FileHTTPHeaders and Metadata.
+// 	err = azfile.UploadFileToAzureFile(ctx, file, fileURL,
+// 		azfile.UploadToAzureFileOptions{
+// 			Parallelism: 3,
+// 			FileHTTPHeaders: azfile.FileHTTPHeaders{
+// 				CacheControl: "no-transform",
+// 			},
+// 			Metadata: azfile.Metadata{
+// 				"createdby": "ananth&gokul",
+// 			},
+// 			// If Progress is non-nil, this function is called periodically as bytes are uploaded.
+// 			Progress: func(bytesTransferred int64) {
+// 				fmt.Printf("Uploaded %d of %d bytes.\n", bytesTransferred, fileSize.Size())
+// 			},
+// 		})
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	return vars
+// }
